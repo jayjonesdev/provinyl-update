82efb2fc4167bf48c8d592d0f43fd05036a56965
Jerrell Jones <jayjonesdevelopment@gmail.com>
2023-10-15 01:01:12 -0500

    move styled components to correct file

--- src/routes/Collection/ReleaseDialogDetails.tsx
+++ src/routes/Collection/ReleaseDialogDetails.tsx
@@ -1,52 +1,9 @@
 import * as React from 'react';
-import { styled } from '@mui/material/styles';
-import ArrowForwardIosSharpIcon from '@mui/icons-material/ArrowForwardIosSharp';
-import MuiAccordion, { AccordionProps } from '@mui/material/Accordion';
-import MuiAccordionSummary, {
-	AccordionSummaryProps,
-} from '@mui/material/AccordionSummary';
-import MuiAccordionDetails from '@mui/material/AccordionDetails';
 import Typography from '@mui/material/Typography';
 import { ReleaseDetails } from '../../helpers/types';
 import { useAppState } from '../../helpers/hooks/useAppState';
 import { Button } from '@mui/material';
-
-const Accordion = styled((props: AccordionProps) => (
-	<MuiAccordion disableGutters elevation={0} square {...props} />
-))(({ theme }) => ({
-	border: `1px solid ${theme.palette.divider}`,
-	'&:not(:last-child)': {
-		borderBottom: 0,
-	},
-	'&:before': {
-		display: 'none',
-	},
-}));
-
-const AccordionSummary = styled((props: AccordionSummaryProps) => (
-	<MuiAccordionSummary
-		expandIcon={<ArrowForwardIosSharpIcon sx={{ fontSize: '0.9rem' }} />}
-		{...props}
-	/>
-))(({ theme }) => ({
-	backgroundColor: theme.palette.secondary.light,
-	color: theme.palette.secondary.contrastText,
-	flexDirection: 'row-reverse',
-	'& .MuiAccordionSummary-expandIconWrapper.Mui-expanded': {
-		transform: 'rotate(90deg)',
-	},
-	'& .MuiAccordionSummary-expandIconWrapper': {
-		color: theme.palette.secondary.contrastText,
-	},
-	'& .MuiAccordionSummary-content': {
-		marginLeft: theme.spacing(1),
-	},
-}));
-
-const AccordionDetails = styled(MuiAccordionDetails)(({ theme }) => ({
-	padding: theme.spacing(2),
-	borderTop: '1px solid rgba(0, 0, 0, .125)',
-}));
+import { Accordion, AccordionDetails, AccordionSummary } from './styles';
 
 const Detail = ({ title, desc }: { title: string; desc: string | number }) => (
 	<Typography variant="body1">
--- src/routes/Collection/styles.tsx
+++ src/routes/Collection/styles.tsx
@@ -14,6 +14,12 @@ import {
 	TableRow,
 	styled,
 } from '@mui/material';
+import ArrowForwardIosSharpIcon from '@mui/icons-material/ArrowForwardIosSharp';
+import MuiAccordion, { AccordionProps } from '@mui/material/Accordion';
+import MuiAccordionSummary, {
+	AccordionSummaryProps,
+} from '@mui/material/AccordionSummary';
+import MuiAccordionDetails from '@mui/material/AccordionDetails';
 import {
 	LazyLoadImage,
 	LazyLoadImageProps,
@@ -180,3 +186,40 @@ export const GridTile = styled('div')`
 		cursor: pointer;
 	}
 `;
+
+export const Accordion = styled((props: AccordionProps) => (
+	<MuiAccordion disableGutters elevation={0} square {...props} />
+))(({ theme }) => ({
+	border: `1px solid ${theme.palette.divider}`,
+	'&:not(:last-child)': {
+		borderBottom: 0,
+	},
+	'&:before': {
+		display: 'none',
+	},
+}));
+
+export const AccordionSummary = styled((props: AccordionSummaryProps) => (
+	<MuiAccordionSummary
+		expandIcon={<ArrowForwardIosSharpIcon sx={{ fontSize: '0.9rem' }} />}
+		{...props}
+	/>
+))(({ theme }) => ({
+	backgroundColor: theme.palette.secondary.light,
+	color: theme.palette.secondary.contrastText,
+	flexDirection: 'row-reverse',
+	'& .MuiAccordionSummary-expandIconWrapper.Mui-expanded': {
+		transform: 'rotate(90deg)',
+	},
+	'& .MuiAccordionSummary-expandIconWrapper': {
+		color: theme.palette.secondary.contrastText,
+	},
+	'& .MuiAccordionSummary-content': {
+		marginLeft: theme.spacing(1),
+	},
+}));
+
+export const AccordionDetails = styled(MuiAccordionDetails)(({ theme }) => ({
+	padding: theme.spacing(2),
+	borderTop: '1px solid rgba(0, 0, 0, .125)',
+}));
